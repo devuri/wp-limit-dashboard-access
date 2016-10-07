fbf8cc8f6569e3715d6eb7abcf68e5817f9ca00f
jrpage <jrpagemedia@gmail.com>
2016-10-07 11:18:41 -0500

    Add files via upload

---
+++ _admin/footer.php
@@ -0,0 +1,9 @@
+<?php 
+
+				echo '<hr/>';
+				echo '<p class="description">';
+				  echo 'This ';
+				  echo get_bloginfo( "name" );
+				  echo ' Plugin powered by <a href="http://wp.devuri.com">Devuri</a>';
+				  echo '</p>';
+				echo '</div>';
---
+++ _admin/head.php
@@ -0,0 +1,12 @@
+<?php 
+
+		
+		// add pop 
+		add_thickbox();
+				echo '<div class="wrap">';
+				//screen_icon(); 
+				echo '<h2>';
+				echo lide_info('name');
+				echo '</h2>';
+				echo'<p class="description"> '.lide_info('name').' - '.lide_info('description').' </p>';
+				// check dependencies
